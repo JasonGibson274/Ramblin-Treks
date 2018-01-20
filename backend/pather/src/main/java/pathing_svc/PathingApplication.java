e378d1ef866b23f71a6557cc67c3fefa49a39eba
Jason Gibson <jgibson37@gatech.edu>
2018-01-20 18:06:04 -0500

    finished configuration of services

--- backend/pather/src/main/java/pathing_svc/PathingApplication.java
+++ backend/pather/src/main/java/pathing_svc/PathingApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 @EnableSwagger2
 public class PathingApplication {
     public static void main(String[] args) {
