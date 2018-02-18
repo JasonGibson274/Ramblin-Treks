f815fecfceb2a906dcfaae90ba81311ac7ca0513
Varun Giridhar <vgiridhar1997@gmail.com>
2018-02-18 14:58:02 -0500

    made POST request to server for path

--- RamblinTreks_iOS/RamblinTreks/Controller/MapViewController.swift
+++ RamblinTreks_iOS/RamblinTreks/Controller/MapViewController.swift
@@ -22,10 +22,20 @@ class MapViewController: UIViewController {
     //Location Manager to get current location
     let locationManager = CLLocationManager()
     
+    //Building Model
+    struct Building {
+        let name : String
+        let address : String
+        let latitude : String
+        let longitude : String
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+        
+//        loadBuildings()
         //Gatech location
         let gaTech = GMSCameraPosition.camera(withLatitude: 33.7756, longitude: -84.3963, zoom: 14)
         //Set map to focus on gatech area
@@ -34,6 +44,77 @@ class MapViewController: UIViewController {
         //Add my current location button
         mapView.isMyLocationEnabled = true
         mapView.settings.myLocationButton = true
+        
+        let completion = {(result: NSString) in print(result)}
+        getPath(startLatitude: "33.7768361", startLongitude: "-84.3897006", endLatitude: "33.776995", endLongitude: "-84.397009", completionHandler: completion)
+        
+        print("********************************************")
+        print("********************************************")
+        print("********************************************")
+        print("COMPLETION")
+        print(completion)
+        print("********************************************")
+        print("********************************************")
+        print("********************************************")
+    }
+    
+    func loadBuildings() {
+        guard let url = URL(string: "https://m.gatech.edu/api/gtplaces/buildings") else {return}
+        
+        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+            if error != nil {
+                print("Error")
+            } else {
+                if let mydata = data {
+                    do {
+                        let myJson = try JSONSerialization.jsonObject(with: mydata) as! [String: Any]
+                        print(type(of: myJson))
+                    } catch {
+                        
+                    }
+                }
+            }
+        }
+        task.resume()
+    }
+    
+    func getPath(startLatitude: String!, startLongitude: String!, endLatitude: String!, endLongitude: String!, completionHandler: @escaping (NSString) -> ()) {
+        let urlString = "http://jasongibson274.hopto.org:9003/pathing"
+        
+        let parameters = ["startLatitude": startLatitude, "startLongitude": startLongitude, "endLatitude": endLatitude, "endLongitude": endLongitude]
+        
+        let myUrl = URL(string: urlString)
+        
+        var request = URLRequest(url: myUrl!)
+        
+        request.httpMethod = "POST"
+        
+        let httpBody = try? JSONSerialization.data(withJSONObject: parameters, options: [])
+        
+        request.httpBody = httpBody
+        
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        
+        let task = URLSession.shared.dataTask(with: request) { (data, response, eror) in
+            if let response = response {
+//                print(response)
+            }
+            
+            if let data = data {
+                do {
+                    let json = try JSONSerialization.jsonObject(with: data, options: [])
+                    print(json)
+                    let responseString = NSString(data: data, encoding: String.Encoding.utf8.rawValue)
+                    completionHandler(responseString!)
+                } catch {
+                    print(error)
+                }
+            }
+        }
+        
+        task.resume()
+        
     }
 
     override func didReceiveMemoryWarning() {
